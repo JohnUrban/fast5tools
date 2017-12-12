ed079053ae1cbc262cf62815e4dbf3e15c761361
John Urban <mr.john.urban@gmail.com>
2017-12-12 17:40:44 -0500

    viz alns

--- fast5tools/barcodeops.py
+++ fast5tools/barcodeops.py
@@ -117,16 +117,30 @@ def get_max_scoring_barcode(alignments, match_prob=0.75):
     return maxbar, maxprob, maxscore, meanscore, scorestring, probs_string, alignments[maxbar].q_pos, alignments[maxbar].q_end, alignments[maxbar].r_pos, alignments[maxbar].r_end, alignments[maxbar].cigar_str #, alignments[maxbar].orig_query,  alignments[maxbar].orig_ref 
 
 
-def choose_barcode(barcodes, sw, read_seq, search_start, search_end, ref_name='', use_entire_barcode=True):
+def choose_barcode(barcodes, sw, read_seq, search_start, search_end, ref_name='', use_entire_barcode=True, return_alns=False):
 
     search_seq = read_seq[search_start:search_end]
 ##    print len(search_seq)
     # get alignments
     alignments = get_alignments(barcodes, sw, search_seq, ref_name, use_entire_barcode)
     #get best barcode info
-    return get_max_scoring_barcode(alignments)
+    if return_alns:
+        return get_max_scoring_barcode(alignments), alignments
+    else:
+        return get_max_scoring_barcode(alignments)
 
-
+def get_formatted_pairwise_alignments(barcodechoice, getall=False):
+    ''' barcode choice = output of choose_barcode() with  return_alns=True'''
+    outstring = ''
+    if getall:
+        for barcode, alignment in barcodechoice[1].iteritems():
+            outstring += get_formatted_pairwise_alignment(alignment)
+        return outstring
+    else:
+        best = barcodechoice[1][barcodechoice[0][0]]
+        return get_formatted_pairwise_alignment(best)
+            
+    
 def get_formatted_pairwise_alignment(alignment, blocksize=100):
     n_m = 0
     n_mm = 0
@@ -163,13 +177,23 @@ def get_formatted_pairwise_alignment(alignment, blocksize=100):
             q_i += e[0]
             n_i += 1
     assert len(ref) == len(query)
-    print "Match:"+str(n_m), "Mismatch:"+str(n_mm), "Deletion:"+str(n_d), "Insertion:"+str(n_i), "PercentIdentity:"+str(100.0*n_m/sum([n_m, n_mm, n_d, n_i]))
-##    print alignment.matches, alignment.mismatches, alignment.mismatches-n_mm-n_d-n_i, len(query), sum([n_m, n_mm, n_d, n_i]),  sum([n_mm, n_d, n_i])
-    print "Ref (top):", alignment.r_name
-    print "Query (bottom):", alignment.q_name
+####    print alignment.matches, alignment.mismatches, alignment.mismatches-n_mm-n_d-n_i, len(query), sum([n_m, n_mm, n_d, n_i]),  sum([n_mm, n_d, n_i])
+##    print "Ref (top):", alignment.r_name
+##    print "Query (bottom):", alignment.q_name
+##    print "Match:"+str(n_m), "Mismatch:"+str(n_mm), "Deletion:"+str(n_d), "Insertion:"+str(n_i), "PercentIdentity:"+str(100.0*n_m/sum([n_m, n_mm, n_d, n_i]))
+##    for i in range(0, len(ref), blocksize):
+##        print ref[i:i+blocksize]
+##        print sticks[i:i+blocksize]
+##        print query[i:i+blocksize]
+##        print
+    outstring = ''
+    outstring += "Ref (top): " + alignment.r_name + '\n'
+    outstring += "Query (bottom): " + alignment.q_name + '\n'
+    stats = ["Match:"+str(n_m), "Mismatch:"+str(n_mm), "Deletion:"+str(n_d), "Insertion:"+str(n_i), "AlignmentLength:"+str(len(ref)), "PercentIdentity:"+str(100.0*n_m/sum([n_m, n_mm, n_d, n_i]))]
+    outstring += (' ').join(stats) + '\n'
     for i in range(0, len(ref), blocksize):
-        print ref[i:i+blocksize]
-        print sticks[i:i+blocksize]
-        print query[i:i+blocksize]
-        print
+        outstring += ref[i:i+blocksize] + '\n'
+        outstring += sticks[i:i+blocksize] + '\n'
+        outstring += query[i:i+blocksize] + '\n\n'
+    return outstring
     
