16d7ab9cce5665f91fc48511e3a2191edbe270b0
John Urban <mr.john.urban@gmail.com>
2017-12-13 14:26:08 -0500

    updated aln viz

--- fast5tools/barcodeops.py
+++ fast5tools/barcodeops.py
@@ -76,6 +76,7 @@ def get_alignments(barcodes, sw, sequence, ref_name='', use_entire_barcode=True)
     return alignments
 
 
+
 def get_max_scoring_barcode(alignments, match_prob=0.75):
     mismatch_prob = 1 - match_prob
     maxscore = float('-inf')
@@ -114,7 +115,7 @@ def get_max_scoring_barcode(alignments, match_prob=0.75):
 ##    print "FINAL:"
 ##    get_formatted_pairwise_alignment(alignments[maxbar])
     ##  alignments[maxbar].identity, alignments[maxbar].matches, alignments[maxbar].mismatches, I, D, M,
-    return maxbar, maxprob, maxscore, meanscore, scorestring, probs_string, alignments[maxbar].q_pos, alignments[maxbar].q_end, alignments[maxbar].r_pos, alignments[maxbar].r_end, alignments[maxbar].cigar_str #, alignments[maxbar].orig_query,  alignments[maxbar].orig_ref 
+    return maxbar, maxprob, maxscore, meanscore, scorestring, probs_string, alignments[maxbar].q_pos, alignments[maxbar].q_end, alignments[maxbar].r_pos, alignments[maxbar].r_end, alignments[maxbar].cigar_str, epowersum #, alignments[maxbar].orig_query,  alignments[maxbar].orig_ref 
 
 
 def choose_barcode(barcodes, sw, read_seq, search_start, search_end, ref_name='', use_entire_barcode=True, return_alns=False):
@@ -134,14 +135,16 @@ def get_formatted_pairwise_alignments(barcodechoice, getall=False):
     outstring = ''
     if getall:
         for barcode, alignment in barcodechoice[1].iteritems():
-            outstring += get_formatted_pairwise_alignment(alignment)
+            outstring += get_formatted_pairwise_alignment(alignment,report_prob=(np.e**alignment.score)/barcodechoice[0][-1])
         return outstring
     else:
         best = barcodechoice[1][barcodechoice[0][0]]
-        return get_formatted_pairwise_alignment(best)
+        return get_formatted_pairwise_alignment(best, report_prob=barcodechoice[0][1])
             
     
-def get_formatted_pairwise_alignment(alignment, blocksize=100):
+def get_formatted_pairwise_alignment(alignment, blocksize=100, report_prob=False, with_unaligned_portion=False):
+    ## TODO: printout unaligned portion as part of alignment viz
+    ## For report_prob, you need to give the prob
     n_m = 0
     n_mm = 0
     n_d = 0
@@ -169,14 +172,13 @@ def get_formatted_pairwise_alignment(alignment, blocksize=100):
             query += '-'*e[0]
             sticks += ' '*e[0]
             r_i += e[0]
-            n_d += 1
+            n_d += e[0] #1
         elif e[1] == 'I':
             ref += '-'*e[0]
             query += alignment.orig_query[q_i:q_i+e[0]]
             sticks += ' '*e[0]
             q_i += e[0]
-            n_i += 1
-    assert len(ref) == len(query)
+            n_i += e[0] #1
 ####    print alignment.matches, alignment.mismatches, alignment.mismatches-n_mm-n_d-n_i, len(query), sum([n_m, n_mm, n_d, n_i]),  sum([n_mm, n_d, n_i])
 ##    print "Ref (top):", alignment.r_name
 ##    print "Query (bottom):", alignment.q_name
@@ -186,11 +188,19 @@ def get_formatted_pairwise_alignment(alignment, blocksize=100):
 ##        print sticks[i:i+blocksize]
 ##        print query[i:i+blocksize]
 ##        print
+    qbases = n_m + n_mm + n_i
+    rbases = n_m + n_mm + n_d
+    assert qbases == alignment.q_end-alignment.q_pos and rbases == alignment.r_end-alignment.r_pos
+    assert len(ref) == len(query)
+    assert len(ref) == sum([n_m, n_mm, n_d, n_i])
     outstring = ''
-    outstring += "Ref (top): " + alignment.r_name + '\n'
-    outstring += "Query (bottom): " + alignment.q_name + '\n'
-    stats = ["Match:"+str(n_m), "Mismatch:"+str(n_mm), "Deletion:"+str(n_d), "Insertion:"+str(n_i), "AlignmentLength:"+str(len(ref)), "PercentIdentity:"+str(100.0*n_m/sum([n_m, n_mm, n_d, n_i]))]
+    outstring += "Ref (top): " + alignment.r_name + ':' + str(alignment.r_pos) + '-' + str(alignment.r_end) + ' r_bases_aligned:' + str(rbases) + ' pct_r_bases_aligned:' + str(100.0*rbases/len(alignment.orig_ref)) + ' refLen:' + str(len(alignment.orig_ref)) + ' bp\n'
+    outstring += "Query (bottom): " + alignment.q_name + ':' + str(alignment.q_pos) + '-' + str(alignment.q_end) + ' q_bases_aligned:' + str(qbases) + ' pct_q_bases_aligned:'+str(100.0*qbases/len(alignment.orig_query)) + ' queryLen:' + str(len(alignment.orig_query)) + ' bp\n'
+    stats = ["AS:" + str(alignment.score), "Match:"+str(n_m), "Mismatch:"+str(n_mm), "Deletion:"+str(n_d), "Insertion:"+str(n_i), "AlignmentLength:"+str(len(ref)), "PercentIdentity:"+str(100.0*n_m/sum([n_m, n_mm, n_d, n_i]))]
     outstring += (' ').join(stats) + '\n'
+
+    if report_prob is not False:
+        outstring += "Marginalized Probability Given Barcode Set: " + str(report_prob) + "\n"
     for i in range(0, len(ref), blocksize):
         outstring += ref[i:i+blocksize] + '\n'
         outstring += sticks[i:i+blocksize] + '\n'
